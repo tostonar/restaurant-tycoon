471b9ca06965cb8ccd5f0475deff44981a88388a
Andrea T <tostonar@gmail.com>
2019-08-28 18:07:50 -0400

    first draft

--- lib/location.rb
+++ lib/location.rb
@@ -1,3 +1,10 @@
 
 class Location
+    @@locations = []
+    def self.all
+        @@locations
+    end
+    def self.all_addresses
+        @@locations.map {|location| location.address}
+    end
 end
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,20 @@
 
 class Restaurant
+    @@restaurants = []
+
+    
+    def initialize
+        
+    end
+   
+    
+    def create_location
+    end
+    def total_rent
+    end
+    def locations
+    end
+    def self.all
+        @@restaurants
+    end
 end
